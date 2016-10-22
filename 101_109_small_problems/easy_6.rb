0fd55863c2a9782d0f9d88e78a59a224cd42d9f5
Fernanda Franco <francodias.fernanda@gmail.com>
2016-10-22 16:20:11 -0400

    More easy 5 exercises

--- 101_109_small_problems/easy_6.rb
+++ 101_109_small_problems/easy_6.rb
@@ -1,10 +1,27 @@
 # cute angles
 
 def dms(angle)
-  degree, remain = angle.divmod(1)
-  minute, remain = (remain.round(4)*60).divmod(1)
-  second, _ = (remain.round(2)*60).divmod(1)
-  degree.to_s + "°" + format("%02d", minute) + "'" + format("%02d", second) + "\""
+  degree, remainder = angle.divmod(1)
+  minute, remainder = (remainder.round(4)*60).divmod(1)
+  second, _ = (remainder.round(4)*60).divmod(1)
+  #degree.to_s + "°" + format("%02d", minute) + "'" + format("%02d", second) + "\""
+  format(%(#{degree}°%02d'%02d"), minute, second)
+end
+
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
+
+def dms(degrees_float)
+  total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  if degrees_float < 0
+    format(%(#{360 + degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
+  else
+    format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
+  end
 end
 
 p dms(30) == %(30°00'00")
@@ -13,3 +30,9 @@ p dms(254.6) == %(254°36'00")
 p dms(93.034773) == %(93°02'05")
 p dms(0) == %(0°00'00")
 p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+p dms(-76.73)
+
+# easy vowels
+
+
+
