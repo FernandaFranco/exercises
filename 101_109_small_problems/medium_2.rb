924a9751fdec5c6e61fdfcc6c04ba544237ec909
Fernanda Franco <francodias.fernanda@gmail.com>
2016-11-03 20:53:11 +0000

    more medium 2 exercises

--- 101_109_small_problems/medium_2.rb
+++ 101_109_small_problems/medium_2.rb
@@ -1,5 +1,27 @@
-def longest_sentence(text)
-  text.split('.').sort {|sentence| sentence.length }
-end
+# def longest_sentence(file)
+#   text = File.read(file)
+#   ordered_sentences = text.split(/[!.?]\s*/).sort_by {|sentence| sentence.split.count }.reverse
+#   puts ordered_sentences[0], ordered_sentences[0].split.count
+# end
 
-p longest_sentence('I am, therefore I exist. You are what you eat. I love Ruby!')
+# longest_sentence('four_score.txt')
+# longest_sentence('frankenstein.txt')
+
+# def longest_paragraph(file)
+#   text = File.read(file)
+#   ordered_paragraphs = text.split(/\n\n/).sort_by {|paragraph| paragraph.split.count }.reverse
+#   puts ordered_paragraphs[0], ordered_paragraphs[0].split.count
+# end
+
+# longest_paragraph('four_score.txt')
+# longest_paragraph('frankenstein.txt')
+
+# def longest_word(file)
+#   text = File.read(file)
+#   ordered_words = text.split(/\W+/).sort_by {|word| word.size }.reverse
+#   puts ordered_words[0], ordered_words[0].size
+# end
+
+# longest_word('four_score.txt')
+# longest_word('frankenstein.txt')
+
