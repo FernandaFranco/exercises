769834e3bddc30b955b249487e0f6eb4a2a01d4b
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-17 19:05:59 -0500

    all easy 1 exercises

--- 130_ruby_foundations_more_topics/easy_1.rb
+++ 130_ruby_foundations_more_topics/easy_1.rb
@@ -171,10 +171,85 @@ def any?(array)
   false
 end
 
-p any?([1, 3, 5, 6]) { |value| value.even? } == true
-p any?([1, 3, 5, 7]) { |value| value.even? } == false
-p any?([2, 4, 6, 8]) { |value| value.odd? } == false
-p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
-p any?([1, 3, 5, 7]) { |value| true } == true
-p any?([1, 3, 5, 7]) { |value| false } == false
-p any?([]) { |value| true } == false
+# p any?([1, 3, 5, 6]) { |value| value.even? } == true
+# p any?([1, 3, 5, 7]) { |value| value.even? } == false
+# p any?([2, 4, 6, 8]) { |value| value.odd? } == false
+# p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+# p any?([1, 3, 5, 7]) { |value| true } == true
+# p any?([1, 3, 5, 7]) { |value| false } == false
+# p any?([]) { |value| true } == false
+
+
+# def all?(collection)
+#   collection.each do |value|
+#     return false unless yield(value)
+#   end
+
+#   true
+# end
+
+# p all?([1, 3, 5, 6]) { |value| value.odd? } == false
+# p all?([1, 3, 5, 7]) { |value| value.odd? } == true
+# p all?([2, 4, 6, 8]) { |value| value.even? } == true
+# p all?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+# p all?([1, 3, 5, 7]) { |value| true } == true
+# p all?([1, 3, 5, 7]) { |value| false } == false
+# p all?([]) { |value| false } == true
+
+# def none?(collection)
+#   collection.each do |value|
+#     return false if yield(value)
+#   end
+
+#   true
+# end
+
+# def none?(collection, &block)
+#   !any?(collection, &block)
+# end
+
+# p none?([1, 3, 5, 6]) { |value| value.even? } == false
+# p none?([1, 3, 5, 7]) { |value| value.even? } == true
+# p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+# p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+# p none?([1, 3, 5, 7]) { |value| true } == false
+# p none?([1, 3, 5, 7]) { |value| false } == true
+# p none?([]) { |value| true } == true
+
+# def one?(collection)
+#   count = 0
+#   collection.each do |value|
+#     count += 1 if yield(value)
+#     return false if count == 2
+#   end
+
+#   count == 1
+# end
+
+# p one?([1, 3, 5, 6]) { |value| value.even? }    == true
+# p one?([1, 3, 5, 7]) { |value| value.odd? }     == false
+# p one?([2, 4, 6, 8]) { |value| value.even? }    == false
+# p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+# p one?([1, 3, 5, 7]) { |value| true }           == false
+# p one?([1, 3, 5, 7]) { |value| false }          == false
+# p one?([]) { |value| true }                     == false
+
+def count(collection)
+  count = 0
+  collection.each do |value|
+    count += 1 if yield(value)
+  end
+
+  count
+end
+
+def count(collection)
+  
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
