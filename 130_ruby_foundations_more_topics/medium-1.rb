0bf8ae1ac489d4f8c3fc72e31a81ca440c14a08b
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-23 12:18:29 -0500

    more medium 1 exercises

--- 130_ruby_foundations_more_topics/medium-1.rb
+++ 130_ruby_foundations_more_topics/medium-1.rb
@@ -1,9 +1,38 @@
-class Device
-  def initialize
-    @recordings = []
-  end
+# class Device
+#   def initialize
+#     @recordings = []
+#   end
 
-  def record(recording)
-    @recordings << recording
-  end
-end
+#   def record(recording)
+#     @recordings << recording
+#   end
+
+#   def listen
+#     record(yield) if block_given?
+#   end
+
+#   def play
+#     puts @recordings.last
+#   end
+# end
+
+# listener = Device.new
+# p listener.listen { "Hello World!" }
+# p listener.listen
+# listener.play # Outputs "Hello World!"
+
+# class TextAnalyzer
+#   def process
+#     # your implementation
+
+#     file = File.open('sample_text.txt', 'r')
+#     yield(file.read)
+#     file.close
+#   end
+# end
+
+# analyzer = TextAnalyzer.new
+# analyzer.process { |file| puts "#{file.split("\n\n").count} paragraphs" }
+# analyzer.process { |file| puts "#{file.split("\n").count} line" }
+# analyzer.process { |file| puts "#{file.split(" ").count} words" }
+
