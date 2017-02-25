968045d4508d62f388ba173f5d31c94e5ca38a38
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-24 22:04:42 -0500

    all files for 3 last exercises

--- 130_ruby_foundations_more_topics/medium_2_testing/test_text.rb
+++ 130_ruby_foundations_more_topics/medium_2_testing/test_text.rb
@@ -5,12 +5,33 @@ require_relative 'text'
 class TextTest < MiniTest::Test
   def setup
     #open
+    @file = File.open('./sample_text.txt', 'r')
+    @text = Text.new(@file.read)
+    @expected_text = expected_text = <<~TEXT
+    Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
+    Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
+    quem e, viverre meuris. Nunc viverre sed messe e condimentum. Suspendisse ornere justo
+    nulle, sit emet mollis eros sollicitudin et. Etiem meximus molestie eros, sit emet dictum
+    dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
+    et verius meuris, et pheretre mi.
+    TEXT
   end
 
   def test_swap
+    actual_text = @text.swap('a', 'e')
+
+    assert_equal(@expected_text, actual_text + "\n")
+  end
+
+  def test_word_count
+    expected_count = @expected_text.split.count
+
+    assert_equal(expected_count, @text.word_count)
   end
 
   def teardown
     #close
+    @file.close
+    puts "File has been closed: #{@file.closed?}"
   end
 end
